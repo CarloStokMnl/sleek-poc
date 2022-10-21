38336a942c5cfb66ffe915f07d72a8a3229d2aba
Carlo Menorias <carlo.menorias@stok.se>
2022-10-21 13:39:02 +0800

    feat: adding husky and also fixes

--- modules/form/script.js
+++ modules/form/script.js
@@ -1,56 +1,61 @@
-'use strict';
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const existingScript = document.querySelector(`script[src="${src}"]`);
 
-function loadScript (src) {
-	return new Promise((resolve, reject) => {
-		const existingScript = document.querySelector('script[src="' + src + '"]');
+    if (existingScript) {
+      // eslint-disable-next-line no-undef
+      if (typeof hbspt === 'undefined' || typeof hbspt.forms === 'undefined') {
+        existingScript.addEventListener('load', () => resolve(existingScript));
+      } else {
+        resolve(existingScript);
+      }
+    } else {
+      const script = document.createElement('script');
 
-		if (existingScript) {
-			if (typeof hbspt === 'undefined' || typeof hbspt.forms === 'undefined') {
-				existingScript.addEventListener('load', () => resolve(existingScript));
-			}
-			else {
-				resolve(existingScript);
-			}
-		}
-		else {
-			const script = document.createElement('script');
+      script.src = src;
 
-			script.src = src;
-
-			document.body.appendChild(script);
-			script.addEventListener('load', () => resolve(script));
-		}
-	});
+      document.body.appendChild(script);
+      script.addEventListener('load', () => resolve(script));
+    }
+  });
 }
 
-const observer = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		if (entry.isIntersecting) {
-			observer.unobserve(entry.target);
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
 
-			loadScript('https://js.hsforms.net/forms/v2.js').then(() => {
-				const config = {
-					portalId: entry.target.dataset.hsFormPortalId,
-					formId: entry.target.dataset.hsFormFormId,
-					target: '#' + entry.target.id
-				};
+        loadScript('https://js.hsforms.net/forms/v2.js').then(() => {
+          const config = {
+            portalId: entry.target.dataset.hsFormPortalId,
+            formId: entry.target.dataset.hsFormFormId,
+            target: `#${entry.target.id}`,
+          };
 
-				if (entry.target.dataset.hsFormRedirectUrl && entry.target.dataset.hsFormRedirectUrl.length) {
-					config.redirectUrl = entry.target.dataset.hsFormRedirectUrl;
-				}
+          if (
+            entry.target.dataset.hsFormRedirectUrl &&
+            entry.target.dataset.hsFormRedirectUrl.length
+          ) {
+            config.redirectUrl = entry.target.dataset.hsFormRedirectUrl;
+          }
 
-				hbspt.forms.create(config);
-			});
-		}
-	});
-}, {rootMargin: '0% 0% 25% 0%'});
+          // eslint-disable-next-line no-undef
+          hbspt.forms.create(config);
+        });
+      }
+    });
+  },
+  { rootMargin: '0% 0% 25% 0%' },
+);
 
 let i = 0;
 
-document.querySelectorAll('[data-hs-form]').forEach(el => {
-	if (!el.id) {
-		el.id = 'sleek-hs-form-' + i++;
-	}
+document.querySelectorAll('[data-hs-form]').forEach((el) => {
+  if (!el.id) {
+    // eslint-disable-next-line no-param-reassign, no-plusplus
+    el.id = `sleek-hs-form-${i++}`;
+  }
 
-	observer.observe(el);
+  observer.observe(el);
 });
